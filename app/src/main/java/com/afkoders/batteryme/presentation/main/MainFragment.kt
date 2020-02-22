37b79cbb78114b29b4256522a83486979dfd85f2
Andrii Chernysh <andrii.chernysh@uptech.team>
2020-02-22 22:15:12 +0200

    Added icons to bottom navigation bar

--- app/src/main/java/com/afkoders/batteryme/presentation/main/MainFragment.kt
+++ app/src/main/java/com/afkoders/batteryme/presentation/main/MainFragment.kt
@@ -1,10 +1,8 @@
 package com.afkoders.batteryme.presentation.main
 
-import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
+import androidx.appcompat.content.res.AppCompatResources
 import com.afkoders.batteryme.R
 import com.afkoders.batteryme.presentation.base.BaseFragmentImpl
-import com.afkoders.batteryme.presentation.test.TestAgreement
 import com.afkoders.batteryme.utils.extensions.widget.makeGone
 import com.afkoders.batteryme.utils.extensions.widget.makeVisible
 import kotlinx.android.synthetic.main.fragment_main.*
@@ -13,8 +11,6 @@ class MainFragment :
     BaseFragmentImpl<MainFragmentAgreement.Presenter, MainFragmentAgreement.View>(R.layout.fragment_main),
     MainFragmentAgreement.View {
 
-    private lateinit var currentFragment: Fragment
-
     override fun setupInputs() {
         bottomNavigationView.setOnNavigationItemSelectedListener {
             sectionBattery.makeGone()
@@ -22,13 +18,38 @@ class MainFragment :
             sectionEvents.makeGone()
             sectionLeaderboard.makeGone()
 
+            val menu = bottomNavigationView.menu
+            menu.findItem(R.id.menuItemBattery).icon =
+                AppCompatResources.getDrawable(requireContext(), R.drawable.ic_battery)
+            menu.findItem(R.id.menuItemEvents).icon =
+                AppCompatResources.getDrawable(requireContext(), R.drawable.ic_events)
+            menu.findItem(R.id.menuItemChallenges).icon =
+                AppCompatResources.getDrawable(requireContext(), R.drawable.ic_challenges)
+            menu.findItem(R.id.menuItemLeaderboard).icon =
+                AppCompatResources.getDrawable(requireContext(), R.drawable.ic_leaderboard)
+
             when (it.itemId) {
-                R.id.menuItemBattery -> sectionBattery
-                R.id.menuItemChallenges -> sectionChallenges
-                R.id.menuItemEvents -> sectionEvents
-                R.id.menuItemLeaderboard -> sectionLeaderboard
-                else -> sectionBattery
-            }.makeVisible()
+                R.id.menuItemBattery -> {
+                    sectionBattery.makeVisible()
+                    it.icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_battery_selected)
+                }
+                R.id.menuItemChallenges -> {
+                    sectionChallenges.makeVisible()
+                    it.icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_challenges_selected)
+                }
+                R.id.menuItemEvents -> {
+                    sectionEvents.makeVisible()
+                    it.icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_events_selected)
+                }
+                R.id.menuItemLeaderboard -> {
+                    sectionLeaderboard.makeVisible()
+                    it.icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_leaderboard_selected)
+                }
+                else -> {
+                    sectionBattery.makeVisible()
+                    it.icon = AppCompatResources.getDrawable(requireContext(), R.drawable.ic_battery_selected)
+                }
+            }
 
             true
         }
