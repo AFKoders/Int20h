cad7897724c7caba87144dafeb0e94752c276f2a
kalevych_oleksandr <kalevych.oleksandr@uptech.team>
2020-02-22 22:39:02 +0200

    fixed layout

--- app/src/main/java/com/afkoders/batteryme/presentation/events/list/EventViewHolder.kt
+++ app/src/main/java/com/afkoders/batteryme/presentation/events/list/EventViewHolder.kt
@@ -15,6 +15,9 @@ import com.afkoders.batteryme.utils.extensions.throttleFirst
 import com.bumptech.glide.Glide
 import com.jakewharton.rxbinding2.view.RxView
 import kotlinx.android.synthetic.main.item_event.view.*
+import java.text.SimpleDateFormat
+import java.time.Month
+import java.time.format.TextStyle
 import java.util.*
 
 class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -37,7 +40,7 @@ class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val calendar = Calendar.getInstance()
         calendar.time = time
         tvEventTime.text = calendar.get(Calendar.HOUR_OF_DAY).toString()+" : "+calendar.get(Calendar.MINUTE).toString()
-        tvEventDate.text = calendar.get(Calendar.DAY_OF_MONTH).toString()
+        tvEventDate.text = calendar.get(Calendar.DAY_OF_MONTH).toString()+"\n"+ SimpleDateFormat("MMM").format(calendar.time)
     }
 
     fun bindUsers(users: List<User>) {
