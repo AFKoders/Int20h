c268e3cd78df132b93078125285cb33a1114cf61
kalevych_oleksandr <kalevych.oleksandr@uptech.team>
2020-02-23 08:58:51 +0200

    chargeboard ttitle

--- app/src/main/java/com/afkoders/batteryme/presentation/leaderboard/LeaderboardPresenter.kt
+++ app/src/main/java/com/afkoders/batteryme/presentation/leaderboard/LeaderboardPresenter.kt
@@ -3,6 +3,7 @@ package com.afkoders.batteryme.presentation.leaderboard
 import com.afkoders.batteryme.data.repository.Repository
 import com.afkoders.batteryme.presentation.base.BasePresenterImpl
 import com.afkoders.batteryme.presentation.common.models.AdapterDelegateItem
+import com.afkoders.batteryme.presentation.common.models.HeaderItem
 import com.afkoders.batteryme.presentation.leaderboard.model.LeaderboardModel
 import javax.inject.Inject
 
@@ -11,28 +12,16 @@ class LeaderboardPresenter @Inject constructor(private val repository: Repositor
     LeaderboardAgreement.Presenter {
     override fun uploadData() {
         // TODO fetch data from firebase
-        /* val leaderboard = listOf(
-             LeaderboardModel("https://www.google.com/url?sa=i&url=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fpeople%2Fgeneric-indian-guy%2Fvideos%2Ftrending&psig=AOvVaw1XcsyiLEgkA7mPmeaj5eiD&ust=1582490352496000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCbuYyC5ucCFQAAAAAdAAAAABAD", "name1", 74),
-             LeaderboardModel("https://www.google.com/url?sa=i&url=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fpeople%2Fgeneric-indian-guy%2Fvideos%2Ftrending&psig=AOvVaw1XcsyiLEgkA7mPmeaj5eiD&ust=1582490352496000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCbuYyC5ucCFQAAAAAdAAAAABAD", "name2", 92),
-             LeaderboardModel("https://www.google.com/url?sa=i&url=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fpeople%2Fgeneric-indian-guy%2Fvideos%2Ftrending&psig=AOvVaw1XcsyiLEgkA7mPmeaj5eiD&ust=1582490352496000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCbuYyC5ucCFQAAAAAdAAAAABAD", "name3", 22),
-             LeaderboardModel("https://www.google.com/url?sa=i&url=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fpeople%2Fgeneric-indian-guy%2Fvideos%2Ftrending&psig=AOvVaw1XcsyiLEgkA7mPmeaj5eiD&ust=1582490352496000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCbuYyC5ucCFQAAAAAdAAAAABAD", "name4", 44),
-             LeaderboardModel("https://www.google.com/url?sa=i&url=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fpeople%2Fgeneric-indian-guy%2Fvideos%2Ftrending&psig=AOvVaw1XcsyiLEgkA7mPmeaj5eiD&ust=1582490352496000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCbuYyC5ucCFQAAAAAdAAAAABAD", "name5", 96),
-             LeaderboardModel("https://www.google.com/url?sa=i&url=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fpeople%2Fgeneric-indian-guy%2Fvideos%2Ftrending&psig=AOvVaw1XcsyiLEgkA7mPmeaj5eiD&ust=1582490352496000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCbuYyC5ucCFQAAAAAdAAAAABAD", "name6", 51),
-             LeaderboardModel("https://www.google.com/url?sa=i&url=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fpeople%2Fgeneric-indian-guy%2Fvideos%2Ftrending&psig=AOvVaw1XcsyiLEgkA7mPmeaj5eiD&ust=1582490352496000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCbuYyC5ucCFQAAAAAdAAAAABAD", "name7", 88)
-         )
-         model = listOf(HeaderItem("Leaderboard"))
-             .plus(leaderboard.map { AdapterDelegateItem.Model(it) })
- */
         repository.getAllUsers { users ->
-            view?.populateData(users.map {
+            view?.populateData(listOf(HeaderItem("Chargeboard")).plus(users.map {
                 AdapterDelegateItem.Model(
                     LeaderboardModel(
                         it.photo,
-                        it.name,
+                        it.givenName,
                         it.score
                     )
                 )
-            })
+            }))
         }
 
     }
