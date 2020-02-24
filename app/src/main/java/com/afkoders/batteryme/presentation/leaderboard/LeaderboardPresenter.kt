ce27a031792bcda42ef870b03374d712f8b0e765
Yaroslav Ovdiienko <Yaroslav.Ovdiienko@Yaroslavs-MBP-work.local>
2020-02-24 17:35:00 +0200

    - add sort for charge board

--- app/src/main/java/com/afkoders/batteryme/presentation/leaderboard/LeaderboardPresenter.kt
+++ app/src/main/java/com/afkoders/batteryme/presentation/leaderboard/LeaderboardPresenter.kt
@@ -14,14 +14,15 @@ class LeaderboardPresenter @Inject constructor(private val repository: Repositor
         // TODO fetch data from firebase
         repository.getAllUsers { users ->
             view?.populateData(listOf(HeaderItem("Chargeboard")).plus(users.map {
-                AdapterDelegateItem.Model(
-                    LeaderboardModel(
-                        it.photo,
-                        it.givenName,
-                        it.score
-                    )
+                LeaderboardModel(
+                    it.photo,
+                    it.givenName,
+                    it.score
                 )
-            }))
+            }
+                .sortedByDescending { it.score }
+                .map { AdapterDelegateItem.Model(it) }
+            ))
         }
 
     }
